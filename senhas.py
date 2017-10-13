c080e369aae93f6ffacd521cd1a1f4483b11c77e
Pedro Guilherme Siqueira Moreira <pedro.eletronica.unb@gmail.com>
2017-10-13 17:03:09 -0300

    Segundo milestone concluído
    Agora é realizado um 'dump' das senhas na ordem
    original e o resultado da requisição POST
    vai para 'resultado.html' (ambos os arquivos são
    ignorados pelo .gitignore).

--- senhas.py
+++ senhas.py
@@ -10,6 +10,7 @@ values = {'nome' : 'Pedro Guilherme Siqueira Moreira'}
 data = urlencode(values)
 full_url = url + '?' + data # http://minha.url.com/etc?param1=val1&param2=val2...
 
+# leitura dos dados
 req = Request(full_url)
 with urlopen(req) as response:
     resp = json.loads(response.read().decode('utf-8')) # o resultado da requisição está em JSON
@@ -18,8 +19,20 @@ with urlopen(req) as response:
     post_to = resp['postTo'] # url de envio POST e os parâmetros necessários
     nome = resp['nome'] # o nome de quem envia
 
+# dump das senhas
+with open('dump_senhas', 'w') as fp:
+    for senha in senhas:
+        fp.write(str(senha) + '\n')
+
+# primeiro milestone
+## ordenando pela hora de emissão
 lista_ordenada = sorted(senhas, key = lambda x: int(x['emissao']))
 
+# segundo milestone
+for i in range(len(lista_ordenada)):
+    lista_ordenada[i]['naFrente'] = i
+
+# requisição POST final
 values = {'nome': nome, 'chave': chave, 'resultado': lista_ordenada}
 data = urlencode(values).encode()
 
